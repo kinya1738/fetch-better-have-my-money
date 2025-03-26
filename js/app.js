47be9675c6209504d83a82b01963c624a3b86c24
kinya1738 <shirleen.njeru@student.moringaschool.com>
2025-03-26 12:32:45 -0700

    complete delete

--- js/app.js
+++ js/app.js
@@ -18,6 +18,7 @@ function fetchTasks() {
 }
 
 function renderTodos(todos) {
+    tasksList.innerHtml = '';
 
     todos.forEach((todo) => {
     const todoElement = document.createElement('div');
@@ -29,6 +30,9 @@ function renderTodos(todos) {
     checkbox.className = 'checkbox'
     checkbox.checked = todo.completed;
 
+    checkbox.addEventListener('change', () =>toggleTodoCompletionStatus(todo.id, todo.completed));
+
+
     const todoText = document.createElement('span');
 
     todoText.textContent = todo.title;
@@ -36,6 +40,12 @@ function renderTodos(todos) {
     const deleteButton = document.createDocumentFragment('button');
     deleteButton.className = 'delete-button';
     deleteButton.innerHtml = '<i class="fa-solid fa-trash"></i>';
+
+    deleteButton.addEventListener("click", () => deleteTodo(todo.id))
+if (todo.completed) {
+    todoText.style.textDecoration = 'line-through';
+    todoElement.style.opacity = '0.3';
+}
 
     todoElement.appendChild(checkbox);
     todoElement.appendChild(todoText);
@@ -50,5 +60,77 @@ function setRemainingTasksCount(todos) {
   const remainingTodos = todos.filter((todo) => !todo.completed).length;
 
   remainingTodosCount.textContent = remainingTodos;
+}
 
+//POST requests 
+const addTodoForm = document.querySelector('#add-task-form');
+ 
+const todoInput = document.querySelector('#task-input');
+
+addTodoForm.addEventListener('submit', CaptureTextInput);
+
+function CaptureTextInput(e) {
+    e.preventDefault();
+
+const text = todoInput.value.trim();
+
+if (text) {
+    addNewTodo(text);
+    e.target.reset();
+}
+}
+
+//async await fetch syntax
+// async function addNewTodo(textFromInput) {
+//    try {
+//     const response = await fetch(API_URL, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({
+//             title: textFromInput,
+//             completed: false,
+//         })
+//     });
+
+// if (!response.ok){
+//     alert(response.status);
+// }
+
+//     fetchTasks();
+//    } catch (error) {
+//     alert(error);
+//    }
+
+async function toggleTodoCompletionStatus(id, completed) {
+    try {
+      const response = await  fetch(`${API_URL}/${id}`, {
+         method: 'PATCH',
+         headers: {
+           'Content-Type': 'application/json',
+         },
+         body: JSON.stringify({
+            completed: !completed,
+         }),
+      })
+      if (!response.ok) {
+        throw new ERR(response);
+      }
+    } catch (error) {
+        alert(error);
+    }
+}
+
+async function deleteTodo(id) {
+    try {
+        const response = await  fetch(`${API_URL}/${id}`, {
+            method: 'PATCH',
+        });
+        if (!response.ok){
+            throw new ERR (response);
+        }
+    } catch (error) {
+        alert(error);
+    }
 }
