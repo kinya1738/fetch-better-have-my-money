9da86d7410b9de4b7de30e5eef0df005cd739f31
kinya1738 <shirleen.njeru@student.moringaschool.com>
2025-03-26 01:39:16 -0700

    added changes

--- js/app.js
+++ js/app.js
@@ -1,15 +1,54 @@
-const API_URL = 'http://localhost:3000/todos';
+const API_URL = 'http://localhost:4000/todos';
 
 
 document.addEventListener('DOMContentLoaded', fetchTasks);
 
-const tasklist = document.querySelector('#tasks-list');
+const tasksList = document.querySelector('#tasks-list');
 
 function fetchTasks() {
     fetch(API_URL).then((res) => {
         if (res.ok) {
-            alert('Response is not ok');
+            alert('res.status');
         }
         return res.json();
-    }).then(todos => console.log(todos));
+    }).then(todos => { 
+        renderTodos(todos);
+        setRemainingTasksCount(todos);
+});
+}
+
+function renderTodos(todos) {
+
+    todos.forEach((todo) => {
+    const todoElement = document.createElement('div');
+
+    todoElement.className = 'task';
+
+    const checkbox =document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.className = 'checkbox'
+    checkbox.checked = todo.completed;
+
+    const todoText = document.createElement('span');
+
+    todoText.textContent = todo.title;
+
+    const deleteButton = document.createDocumentFragment('button');
+    deleteButton.className = 'delete-button';
+    deleteButton.innerHtml = '<i class="fa-solid fa-trash"></i>';
+
+    todoElement.appendChild(checkbox);
+    todoElement.appendChild(todoText);
+    todoElement.appendChild(deleteButton);
+
+    tasksList.appendChild(todoElement);
+    });
+}
+
+const remainingTodosCount = document.querySelector('#tasks-count');
+function setRemainingTasksCount(todos) {
+  const remainingTodos = todos.filter((todo) => !todo.completed).length;
+
+  remainingTodosCount.textContent = remainingTodos;
+
 }
