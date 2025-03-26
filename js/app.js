55821a771550657874a2ec705a41166c9d6845b7
kinya1738 <shirleen.njeru@student.moringaschool.com>
2025-03-26 10:37:46 -0700

    initial-commit

--- js/app.js
+++ js/app.js
@@ -1,2 +1,15 @@
 const API_URL = 'http://localhost:3000/todos';
 
+
+document.addEventListener('DOMContentLoaded', fetchTasks);
+
+const tasklist = document.querySelector('#tasks-list');
+
+function fetchTasks() {
+    fetch(API_URL).then((res) => {
+        if (res.ok) {
+            alert('Response is not ok');
+        }
+        return res.json();
+    }).then(todos => console.log(todos));
+}
